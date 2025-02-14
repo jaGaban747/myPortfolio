9d74845ffaf6b1f7b6fd43ec7556fb106b0fff7a
NeuralLeon <leon.changara@gmail.com>
2025-02-14 09:44:32 -0800

    Fix projects page and dependencies

---
+++ app/projects/page.js
@@ -0,0 +1,9 @@
+// app/projects/page.js
+export default function Projects() {
+    return (
+      <div>
+        <h1>Projects Page</h1>
+        <p>This is the projects page.</p>
+      </div>
+    );
+  }
